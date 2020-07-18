849316e59c18445b54b1e2b292236ee43d3ed6c6
Devmadia <agathaphan@icloud.com>
2020-07-18 02:27:42 -0500

    created markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,60 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // destructuring the array
+  const {
+      title,
+      description,
+      installation,
+      usage,
+      contribution,
+      test,
+      license,
+      github,
+      email
+  } = data;
+    
+  /* Questions: WHEN I enter my GitHub username
+  THEN this is added to the section of the README 
+  entitled Questions, with a link to my GitHub profile */
 
+  return `# ${data.title}
+  # $(title)
+
+  $(license)
+
+  ## Description
+
+  $(description)
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [Questions](#questions) 
+  
+  ## Installation
+  $(installation)
+
+  ##Usage
+  $(usage)
+
+  ## License
+  This project is covered by the $(license) license.
+
+  ## Contribution
+  $(contribution)
+
+  ## Test
+  $(test)
+
+  ## Questions
+  Visit my GitHub 
+
+  If you have any additional questions not answered here or wish to contact me regarding developments, please email me at $(email)
+   
 `;
 }
 
