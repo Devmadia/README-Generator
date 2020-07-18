144a57a7f7a64a8bb4074bca363efe6853f2d58e
Devmadia <agathaphan@icloud.com>
2020-07-18 02:16:36 -0500

    refined questions based on development criteria

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
+const inquirer = require('inquirer');
+// // receiving module.exports from generateMarkdown.js
+// const generatePage = require('./src/generateMarkdown.js');
+
 /* Questions: the title of project and sections entitled "Description", "Table of Contents", 
 "Installation", "Usage", "License", "Contributing", "Tests", and "Questions" */
 
+// in order to use File System, the below constant is necessary
+const fs = require('fs');
 
 // array of questions for user
 const questions = [
     {
+        // README.md title
         type: "input",
-        name: "username",
-        message: "What is your GitHub username? (Required)",
-        validate: usernameInput => {
-            if (usernameInput) {
+        name: "title",  
+        message: "What is the title of your project? (Required)",
+        validate: titleInput => {
+            if (titleInput) {
                 return true;
             } else {
-                console.log("Please enter your GitHub username!");
-                return false;
-            }
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "what is your email address?",
-        default: true
-    },
-    {
-        type: "input",
-        name: "projectname",
-        message: "What is your project's name? (Required)",
-        validate: projectnameInput => {
-            if (projectnameInput) {
-                return true;
-            } else {
-                console.log("Please enter your project's name!");
+                console.log("Please enter the title of your project!");
                 return false;
             }
         }
@@ -39,7 +27,7 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Please write a short description of your project. (Required)",
+        message: "Please provide a short description of your project. (Required)",
         validate: descriptionInput => {
             if (descriptionInput) {
                 return true;
@@ -50,14 +38,33 @@ const questions = [
     },
     {
         type: "input",
-        name: "license",
-        message: "What kind of license should your project have?",
+        name: "installation",
+        message: "What are the installation instructions for your project?",
         default: true
     },
     {
+        /* a badge for that license is added near the top of the README and 
+        a notice is added to the section of the README entitled License that 
+        explains which license the application is covered under */
         type: "input",
-        name: "command",
-        message: "What command should be run to install dependencies?",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: [
+            "MIT",
+			"GNU AGPLv3",
+			"GNU GPLv3",
+			"GNU LGPLv3",
+			"Mozilla Public 2.0",
+			"Apache 2.0",
+			"Boost Software 1.0",
+			"The Unlicense"
+        ],
+        // default: 0
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "What are the contribution guidelines?",
         default: true
     },
     {
@@ -67,16 +74,34 @@ const questions = [
         default: true
     },
     {
+        /* added to the section of the README entitled Questions, 
+        with a link to a GitHub profile*/
         type: "input",
-        name: "faq",
-        message: "What does the user need to know about using the repo?",
-        default: true
+        name: "github", 
+        message: "What is your GitHub username? (Required)",
+        validate: (githubInput) => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username to continue.");
+                return false;
+            }
+        }
     },
     {
+        /* added to the section of the README entitled Questions, 
+        with instructions on how to reach me with additional questions*/
         type: "input",
-        name: "contributions",
-        message: "What does the user need to know about contributing to the repo?",
-        default: true
+        name: "email",
+        message: "What is your email address? (Required)",
+        validate: (emailInput) => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your email address to contine.");
+                return false;
+            }
+        }
     }
 ];
 
